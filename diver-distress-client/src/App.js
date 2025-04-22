54cdafbdc841962b93fcb1ed95cd938dda1f63cd
NoaHalali <noa4000@gmail.com>
2025-04-22 23:19:45 +0300

    Added NavigationBar component and update App layout

--- diver-distress-client/src/App.js
+++ diver-distress-client/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SensorDashboard from "./components/SensorDashboard";
 import Homepage from "./components/Homepage";
 import DiverDetails from "./components/DiverDetails";
+import NavigationBar from "./components/NavigationBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/group/:groupId" element={<SensorDashboard />} />
-          <Route path="/diver/:diverId" element={<DiverDetails />} />
-        </Routes>
+        <NavigationBar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/group/:groupId" element={<SensorDashboard />} />
+            <Route path="/diver/:diverId" element={<DiverDetails />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
---
+++ diver-distress-client/src/components/NavigationBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+  return (
+    <nav className="navigation-bar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/settings">Settings</Link></li>
+        <li><Link to="/help">Help</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
